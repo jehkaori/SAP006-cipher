465bb64705fdbbd8a2d6f3e173ae4b55899270d5
Jéssica Inamassu <inamassu.kaori@gmail.com>
2021-06-14 18:02:10 -0300

    Cifra pronta

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,48 @@
 const cipher = {
-  // ...
-};
-
-export default cipher;
+  encode: function (string, offset){
+    offset = parseInt(offset, 10)
+    let result = "", message
+   
+      for (let i=0; i<string.length; i++){
+        if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+          message = (((string.charCodeAt(i)-65)+ offset)%26)+65
+          result+=String.fromCharCode(message)
+        }
+        else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+          message = (((string.charCodeAt(i)-97)+ offset)%26)+97
+          result+=String.fromCharCode(message)
+        }
+        else if (string.charCodeAt(i)===32){
+          message = 32
+          result+=String.fromCharCode(message)
+        }
+        else {
+          result = "Escreva" 
+        }
+      }
+      return result
+  },
+  decode: function (string, offset){
+    offset = parseInt(offset, 10)
+    let result = "", message
+    
+    for (let i=0; i<string.length; i++){
+      if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+        message = (((string.charCodeAt(i)-90)- offset)%26)+90
+        result+=String.fromCharCode(message)
+      }
+      else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+        message = (((string.charCodeAt(i)-122)- offset)%26)+122
+        result+=String.fromCharCode(message)
+      }
+      else if (string.charCodeAt(i)===32){
+        message = 32
+        result+=String.fromCharCode(message)
+      }
+      else {
+        result = "Escreva" 
+      }
+    }
+    return result
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-import cipher from './cipher.js';
+/*import cipher from './cipher.js';
 
 console.log(cipher);
+
+function encriptar(elemento){
+    let mensagem = document.getElementById('textoriginal')
+    let desloc = document.getElementsByName('Deslocamento')
+    let mensagemCifrada = ((codigoDaLatraASC - codigoLetra + desloc) % tamDoAlfabeto)+ codigoLetra
+    
+  }
+  
+  function desencriptar (elemento){
+}
+   let string = document.getElementById("txtoriginal")
+  let offset = document.getElementById("desloc")
+
+  export default cipher
+
+  /*
+  decode: function decode(string, offset){
+    offset = parseInt(offset, 10)
+    let result = "", message
+    //((codigoDaLatraASC - codigoLetra + desloc) % tamDoAlfabeto) + codigoLetra
+  
+      for (let i=0; i<string.length; i++){
+        if(string.charCodeAt(i)>=90 && string.charCodeAt(i)<=65){
+        message = ((string.charCodeAt(i)-65 - desloc)%26)+65
+        }
+        else if (string.charCodeAt(i)>=122 && string.charCodeAt(i)<=97){
+        message = ((string.charCodeAt(i)-97 - desloc)%26)+122
+        }
+        else if (string.charCodeAt(i)===32){
+          message = 32
+        }
+        result+=String.fromCharCode(message)
+      }
+      return result
+    }
+  
+} 
+export default cipher
+
+  */
+
+  
