0eb3a1da19f8744eccd5530da6aa928844d832a1
Jéssica Inamassu <inamassu.kaori@gmail.com>
2021-06-20 21:54:47 -0300

    README feito

--- src/cipher.js
+++ src/cipher.js
@@ -16,9 +16,29 @@ const cipher = {
           message = 32
           result+=String.fromCharCode(message)
         }
+        else if (string.charCodeAt(i)>=33 && string.charCodeAt(i)<=47){
+          message = (((string.charCodeAt(i)-33)+ offset)%15)+33
+          result+=String.fromCharCode(message)
+        }
+        else if (string.charCodeAt(i)>=48 && string.charCodeAt(i)<=57){
+          message = (((string.charCodeAt(i)-48)+ offset)%10)+48
+          result+=String.fromCharCode(message)
+        }
+        else if (string.charCodeAt(i)>=58 && string.charCodeAt(i)<=64){
+          message = (((string.charCodeAt(i)-58)+ offset)%7)+58
+          result+=String.fromCharCode(message)
+        }
+        else if (string.charCodeAt(i)>=91 && string.charCodeAt(i)<=96){
+          message = (((string.charCodeAt(i)-91)+ offset)%6)+91
+          result+=String.fromCharCode(message)
+        }
+        else if (string.charCodeAt(i)>=123 && string.charCodeAt(i)<=126){
+          message = (((string.charCodeAt(i)-123)+ offset)%4)+123
+          result+=String.fromCharCode(message)
+        }
         else {
-          result = "Escreva" 
-        
+
+          result = "CARACTER INVÁLIDO" 
         }
       }
       return result
@@ -38,10 +58,30 @@ const cipher = {
       }
       else if (string.charCodeAt(i)===32){
         message = 32
+        result+=String.fromCharCode(message) 
+      }
+      else if (string.charCodeAt(i)>=33 && string.charCodeAt(i)<=47){
+        message = (((string.charCodeAt(i)-47)- offset)%15)+47
+        result+=String.fromCharCode(message)
+      }
+      else if (string.charCodeAt(i)>=48 && string.charCodeAt(i)<=57){
+        message = (((string.charCodeAt(i)-57)- offset)%10)+57
+        result+=String.fromCharCode(message)
+      }
+      else if (string.charCodeAt(i)>=58 && string.charCodeAt(i)<=64){
+        message = (((string.charCodeAt(i)-64)- offset)%7)+64
+        result+=String.fromCharCode(message)
+      }
+      else if (string.charCodeAt(i)>=91 && string.charCodeAt(i)<=96){
+        message = (((string.charCodeAt(i)-96)- offset)%6)+96
+        result+=String.fromCharCode(message)
+      }
+      else if (string.charCodeAt(i)>=123 && string.charCodeAt(i)<=126){
+        message = (((string.charCodeAt(i)-126)- offset)%4)+126
         result+=String.fromCharCode(message)
       }
       else {
-        result = "Escreva" 
+        result = "CARACTER INVÁLIDO" 
       }
     }
     return result
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import cipher from './cipher.js';
 
-//const botaoCriarmsg = document.getElementById("Iniciar");
-//const botaoDecifrarmsg = document.getElementById("Ler mensagem");
-//const botaoPag = document.getElementById("Página");
 const offsetmsg = document.getElementById("desloc");
-//const valor = offsetmsg.options[offsetmsg.selectedIndex].text;
 const msgoriginal = document.getElementById("txtoriginal");
-const msgcriptada = document.getElementById("txt");
 const botaoEncriptar = document.getElementById ("encriptar");
-const msgencrip = document.getElementById("txt1")
-const msgdecrip = document.getElementById("txt2")
-const botaoDecriptar = document.getElementById ("decriptar");
+const msgcriptada = document.getElementById("txtcodificado");
+const botaoDecriptar = document.getElementById ("decriptar")
+;
 
 function encrip(){
   const offset = parseInt(offsetmsg.value); 
@@ -22,12 +17,12 @@ function encrip(){
 botaoEncriptar.addEventListener ("click", encrip);
 
 
-function decode (){
+function decrip (){
   const offset = parseInt(offsetmsg.value);
-  const string = msgencrip.value;
-  msgencrip.value = "";
-  msgdecrip.value = cipher.decode(offset,string)
+  const string = msgcriptada.value;
+  msgcriptada.value = "";
+  msgoriginal.value = cipher.decode(offset,string)
 }
-botaoDecriptar.addEventListener ("click",decode)
+botaoDecriptar.addEventListener("click",decrip)
 
 console.log(cipher);
